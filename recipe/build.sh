426854131b79dcdf8c930edbaaeb456a6e2b5bc2
Ray Donnelly <mingw.android@gmail.com>
2018-07-03 15:08:02 -0400

    Ensure m4 is just m4, not $BUILD_PREFIX/bin/m4

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,13 @@
 
 export PERL=${BUILD_PREFIX}/bin/perl
 
-./configure --prefix="$PREFIX" --host=${HOST}
+M4=m4 \
+  ./configure --prefix="$PREFIX" --host=${HOST}
 make -j${CPU_COUNT} ${VERBOSE_AT}
 
 make check
 make install
+
+strings $PREFIX/bin/bison | grep ${BUILD_PREFIX}/bin/m4 || exit 0
+echo "ERROR :: BUILD_PREFIX of ${BUILD_PREFIX}/bin/m4 found in $PREFIX/bin/bison"
+exit 1
